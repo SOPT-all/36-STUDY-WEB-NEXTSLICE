fbb3c1cfd4017e6b9c80dfe1d14130da37ed4319
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-12 18:51:20 +0900

    feat: 간단한 투두리스트

---
+++ practice/week3/routing-test/src/app/week3_test/AddItemForm.tsx
@@ -0,0 +1,35 @@
+// app/routing-test/AddItemForm.tsx
+"use client";
+
+import { useState, useTransition } from "react";
+import { addItem } from "@/actions/addItem";
+
+export default function AddItemForm({
+  onAdd,
+}: {
+  onAdd: (items: string[]) => void;
+}) {
+  const [input, setInput] = useState("");
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmit = () => {
+    startTransition(async () => {
+      const updated = await addItem(input);
+      onAdd(updated);
+      setInput("");
+    });
+  };
+
+  return (
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="추가할 항목 입력"
+      />
+      <button onClick={handleSubmit} disabled={isPending}>
+        {isPending ? "추가 중..." : "추가"}
+      </button>
+    </div>
+  );
+}
---
+++ practice/week3/routing-test/src/app/week3_test/page.tsx
@@ -0,0 +1,25 @@
+// app/routing-test/page.tsx
+"use client";
+import AddItemForm from "./AddItemForm";
+import { useState } from "react";
+import { fakeDB } from "@/lib/db";
+
+export default function RoutingTestPage() {
+  const [items, setItems] = useState(fakeDB);
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>✅Todo-List</h1>
+
+      <ul>
+        {items.map((item, i) => (
+          <li key={i}>📌 {item}</li>
+        ))}
+      </ul>
+
+      <hr style={{ margin: "20px 0" }} />
+
+      <AddItemForm onAdd={setItems} />
+    </div>
+  );
+}

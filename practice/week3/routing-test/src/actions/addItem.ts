750ac69c0acc8b6a9bf72fe0b52d71c33b0a1c46
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-12 19:04:07 +0900

    refactor: 경로 수정

--- practice/week3/routing-test/src/actions/addItem.ts
+++ practice/week3/routing-test/src/actions/addItem.ts
@@ -1,7 +1,7 @@
 //actions/addItem.ts
 "use server";
 
-import { fakeDB } from "@/lib/db";
+import { fakeDB } from "../lib/db";
 
 export async function addItem(newItem: string) {
   fakeDB.push(newItem);

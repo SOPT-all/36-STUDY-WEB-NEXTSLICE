58893136a1272e50ee6ea429e251349bc249e813
soyyyyy <da5868@naver.com>
2025-05-26 21:36:55 +0900

    chore: 브랜치 변경

--- practice/week4/soy-practice/src/app/page.tsx
+++ practice/week4/soy-practice/src/app/page.tsx
@@ -2,19 +2,6 @@ import { getTodos } from './actions'
 import TodoList from './components/TodoList'
 import AddTodo from './components/AddTodo'
 
-const styles = {
-  container: {
-    maxWidth: '500px',
-    margin: '0 auto',
-    padding: '20px',
-    fontFamily: 'Arial, sans-serif',
-  },
-  heading: {
-    textAlign: 'center' as const,
-    marginBottom: '20px',
-  },
-};
-
 export default async function Home() {
 
   // 서버 컴포넌트에서 데이터 가져오기
@@ -31,3 +18,16 @@ export default async function Home() {
     </main>
   )
 }
+
+const styles = {
+  container: {
+    maxWidth: '500px',
+    margin: '0 auto',
+    padding: '20px',
+    fontFamily: 'Arial, sans-serif',
+  },
+  heading: {
+    textAlign: 'center' as const,
+    marginBottom: '20px',
+  },
+};
